f584784ebf0012958afad2811058e5d2c7c9cc21
Jelle Maas <typiqally@gmail.com>
2024-08-08 14:58:17 +0200

    Add culture info to attribute

--- Tpcly.Extensions.Json/DateTimeOffsetFormatAttribute.cs
+++ Tpcly.Extensions.Json/DateTimeOffsetFormatAttribute.cs
@@ -1,11 +1,12 @@
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 namespace Tpcly.Extensions.Json;
 
-public class DateTimeOffsetFormatAttribute(string format) : JsonConverterAttribute
+public class DateTimeOffsetFormatAttribute(string format, CultureInfo? cultureInfo = null) : JsonConverterAttribute
 {
     public override JsonConverter CreateConverter(Type typeToConvert)
     {
-        return new DateTimeFormatJsonConverter(format);
+        return new DateTimeFormatJsonConverter(format, cultureInfo);
     }
 }
--- Tpcly.Extensions.Json/DecimalFormatAttribute.cs
+++ Tpcly.Extensions.Json/DecimalFormatAttribute.cs
@@ -1,11 +1,12 @@
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 namespace Tpcly.Extensions.Json;
 
-public class DecimalFormatAttribute(string format) : JsonConverterAttribute
+public class DecimalFormatAttribute(string format, CultureInfo? cultureInfo = null) : JsonConverterAttribute
 {
     public override JsonConverter CreateConverter(Type typeToConvert)
     {
-        return new DateTimeFormatJsonConverter(format);
+        return new DateTimeFormatJsonConverter(format, cultureInfo);
     }
 }
